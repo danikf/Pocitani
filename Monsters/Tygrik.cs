403bca06859f4a14752eaa3e118afca2261bdc5a
danikf <daniel.frantik@gmail.com>
2017-07-29 01:53:12 +0200

    Tygříček

---
+++ Monsters/Tygrik.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Počítání.Examples;
+
+namespace Počítání.Monsters
+{
+    /// <summary>
+    /// Preferuje rovnice
+    /// </summary>
+    public class Tygrik : BaseMonster
+    {
+        public override int GetExampleFrequencyByMonster(ExampleDef exampleDef)
+        {
+            if (exampleDef.Code.Contains("XEquals") || exampleDef.Code.Contains("EqualsX") || exampleDef.Code.Contains("XPlus"))
+                return 10;
+            else
+                return base.GetExampleFrequencyByMonster(exampleDef);
+        }
+    }
+}
